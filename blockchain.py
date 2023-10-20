288f0cdedf8a268f0f154b7208d0f44f0a722080
Abubakarraza <abubakarraza64@gmail.com>
2023-10-21 02:36:22 +0500

    create ledger handler block chain

---
+++ blockchain.py
@@ -0,0 +1,64 @@
+import datetime
+import hashlib
+import json
+
+
+class BlockChain:
+    def __init__(self):
+        self.chains = []
+        # Genesis block
+        self.createBlock(owner='creator', Reg_no="00",
+                         proof=00, prev_hash='genesis_hash')
+    # create block function
+
+    def createBlock(self, owner, Reg_no, proof, prev_hash):
+        block = {
+            "owner": owner,
+            "Reg_no": Reg_no,
+            "proof": proof,
+            "pev_hash": prev_hash,
+            "index": len(self.chains)+1,
+            "timestamp": str(datetime.datetime.now())
+        }
+        block['hash'] = self.hash(block)
+        self.chains.append(block)
+        return block
+
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash = hashlib.sha256(
+                str(new_proof**2-previous_proof**2).encode()).hexdigest()
+
+            if hash[:3] == "000":
+                check_proof = True
+            else:
+                new_proof += 1
+        print("new_proof", new_proof)
+        return new_proof
+
+    def hash(self, block):
+        encode_block = json.dumps(block).encode()
+        return hashlib.sha256(encode_block).hexdigest()
+
+    def is_valid_chain(self, chain):
+        previous_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            hash = hashlib.sha256(
+                str(proof**2-previous_proof**2).encode()).hexdigest()
+            if hash[:3] != '000':
+                return False
+            previous_block = block
+            block_index += 1
+        return True
+
+    def previous_block(self):
+
+        return self.chains[-1]
--- main.py
+++ main.py
@@ -1,23 +1,66 @@
-import datetime
+
+from flask import Flask, jsonify, request
+from blockchain import BlockChain
 
 
-class BlockChain:
-    def __init__(self):
-        self.chains = []
-        # Genesis block
-        self.createBlock(owner='creator', Reg_no="00",
-                         proof="00", prev_hash="98")
-    # create block function
+# Flask app
+app = Flask(__name__)
 
-    def createBlock(self, owner, Reg_no, proof, prev_hash):
-        block = {
-            "owner": owner,
-            "Reg_no": Reg_no,
-            "proof": proof,
-            "pev_hash": prev_hash,
-            "index": len(self.chains)+1,
-            "timestamp": str(datetime.datetime.now())
+block = BlockChain()
 
+
+@app.route('/')
+def index():
+    return "Welcome to Blockchain"
+
+
+@app.route("/chain", methods=["GET"])
+def chain():
+    response = {
+        "chain": block.chains,
+        "length": len(block.chains)
+    }
+    return jsonify(response), 200
+
+
+@app.route("/is_valid", methods=["GET"])
+def isValid():
+    is_valid = block.is_valid_chain(block.chains)
+    response = {}
+    if is_valid:
+        response = {
+            "message": "The blockchain is valid."
         }
-        self.chains.append(block)
-        return block
+    else:
+        response = {
+            "message": "The blockchain is not valid."
+        }
+
+    return jsonify(response), 200
+
+
+@app.route("/mine", methods=["POST"])
+def mine():
+    data = request.get_json()
+    required = ['owner', 'Reg_no']
+    if not all(key in data for key in required):
+        return "Missing values", 400
+    previous_block = block.previous_block()
+    previous_proof = previous_block['proof']
+    print("previous_proof", previous_proof)
+    proof = block.proof_of_work(previous_proof)
+    previous_hash = block.hash(previous_block)
+    block.createBlock(owner=data['owner'], Reg_no=data['Reg_no'],
+                      proof=proof, prev_hash=previous_hash)
+    response = {
+        "message": "New block created",
+        "index": previous_block['index']+1,
+        "owner": data['owner'],
+        "Reg_no": data['Reg_no'],
+        "proof": proof,
+        "previous_hash": previous_hash
+    }
+    return jsonify(response), 200
+
+
+app.run(debug=True, port=5000)
